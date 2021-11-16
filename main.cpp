a4f1f6e16e44d0fcbf4ef9548ac8051a41a727c1
Saipranav Janyavula <sjanyavu@umich.edu>
2021-11-15 23:28:29 -0500

    Add wgs84 + Eigen

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <array>
 #include "json.hpp"
+#include "WGS84toCartesian.hpp"
+#include "Eigen/Dense"
 
 using json = nlohmann::json;
 using namespace std;
@@ -25,6 +28,36 @@ json generateItem(json waypoint, json pylon, int sequence) {
 
 json rectangularPath(json launch, json pylon1, json pylon2, json mast, int margin = 5) {
     //units in meters, lat/long = coordinates
+    //can't just treat it like x and y b/c the earth is round!!
+    //not sure if cartesian coordinates are also in meters... functions relative to reference
+    //todo figure out actual units
+    //todo best way to do geographic Coordinate Math
+    array<double, 2> roundLaunch = {launch["Latitude"], launch["Longitude"]};
+    array<double, 2> cartLaunch = wgs84::toCartesian(roundLaunch, roundLaunch); //reference, convert
+
+    array<double, 2> roundPylon1 = {pylon1["Latitude"], pylon1["Longitude"]};
+    array<double, 2> cartPylon1 = wgs84::toCartesian(roundLaunch, roundPylon1); //reference, convert
+
+    array<double, 2> roundPylon2 = {pylon2["Latitude"], pylon2["Longitude"]};
+    array<double, 2> cartPylon2 = wgs84::toCartesian(roundLaunch, roundPylon2); //reference, convert
+
+    array<double, 2> roundMast = {mast["Latitude"], mast["Longitude"]};
+    array<double, 2> cartMast = wgs84::toCartesian(roundLaunch, roundMast); //reference, convert
+
+    //as a test
+    cout << "roundpylon1: " << roundPylon1[0] << roundPylon1[1] << endl;
+    cout << "cartpylon1: " << cartPylon1[0] << cartPylon1[1] << endl;
+    //should be able to do normal math on cartesian coordinates
+    //using vector equations to easily get points along the line
+
+    //convert back to wgs84 coordinates (system used by gps)
+    //todo: find documentation on QGroundControl's coordinate system
+    Eigen::Vector2d vecLaunch(cartLaunch[0], cartLaunch[1]);
+    Eigen::Vector2d vecLaunch(cartLaunch[0], cartLaunch[1]);
+
+    //return new path
+    json j1;
+    return j1;
 
 }
 
@@ -33,6 +66,8 @@ int main(int argc, char *argv[]) {
         std::cout << "Usage: MAAV_Path_Generator [input].plan [output].plan [type]" << std::endl;
         return 1;
     }
+    std::cout << "Warning: May not properly account for curvature of the earth" << endl;
+    cout << "Warning: Does not consider vertical distances/optimizations" << endl;
     string inputFile = argv[1];
     string outputFile = argv[2];
     string pathType = argv[3];
@@ -70,7 +105,8 @@ int main(int argc, char *argv[]) {
     cout << "mast: " << mast << endl;
 
     if (pathType == "Rectangular") {
-    //    plan["mission"]["items"] = rectangularPath(launch, pylon1, pylon2, mast);
+        //looks like cartesian coordinates are in meters
+        plan["mission"]["items"] = rectangularPath(launch, pylon1, pylon2, mast);
     } else if (pathType == "Circular") {
         cout << "Circular paths are currently unsupported" << endl;
         return 1;
